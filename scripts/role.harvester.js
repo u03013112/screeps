48359aac85427e6b10a74139b42a91cfac4ea7f0
u03013112 <u03013112@hotmail.com>
2023-11-08 17:05:13 +0800

    负载

--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -5,12 +5,18 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
-            var source0 = source.getSource(creep);
+            if (creep.memory.source == undefined) {
+                creep.memory.source = source.getSource(creep);
+            }
+            var source0 = creep.memory.source;
             if(creep.harvest(source0) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source0, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
+            if (creep.memory.source != undefined) {
+                delete creep.memory.source;
+            }
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
