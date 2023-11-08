917bdad712d8a4294710e278cf73c520d0958558
u03013112 <u03013112@hotmail.com>
2023-11-08 16:43:28 +0800

    js不擅长

--- scripts/data.nameManager.js
+++ scripts/data.nameManager.js
@@ -1,20 +1,20 @@
 // 所有的名字都在这里选，目前就是简单的顺序挑选
 
-harvesterNameList = ['harvester1', 'harvester2', 'harvester3', 'harvester4'];
-upgraderNameList = ['upgrader1', 'upgrader2', 'upgrader3', 'upgrader4'];
-
 var nameManager = {
+    harvesterNameList:['harvester1', 'harvester2', 'harvester3', 'harvester4'],
+    upgraderNameList:['upgrader1', 'upgrader2', 'upgrader3', 'upgrader4'],
+
     /** @param {string} role :类似 harvester or upgrader**/ 
     getName: function(role) {
         if (role == 'harvester') {
-            for (var name in harvesterNameList) {
+            for (var name in this.harvesterNameList) {
                 if (!Game.creeps[name]) {
                     return name;
                 }
             }
         }
         if (role == 'upgrader') {
-            for (var name in upgraderNameList) {
+            for (var name in this.upgraderNameList) {
                 if (!Game.creeps[name]) {
                     return name;
                 }
