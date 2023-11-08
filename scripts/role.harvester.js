59916a6c7b996677723b6db9916a20bf102dbaf5
u03013112 <u03013112@hotmail.com>
2023-11-08 17:29:09 +0800

    debug

--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -6,7 +6,7 @@ var roleHarvester = {
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
             var source0 = source.getSource2(creep);
-            console.log(creep.name + ":" + source0.id)
+            console.log(creep.name + ":" + source0)
             if(creep.harvest(source0) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source0, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
