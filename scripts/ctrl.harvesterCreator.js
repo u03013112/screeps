5c6bed48a33ecb1d84dc54c722055a2304868ceb
u03013112 <u03013112@hotmail.com>
2023-11-08 16:33:16 +0800

    fix bug

--- scripts/ctrl.harvesterCreator.js
+++ scripts/ctrl.harvesterCreator.js
@@ -5,6 +5,10 @@ var harvesterCreator = {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         if(harvesters.length < maxCout) {
             var newName = nameManager.getName('harvester');
+            if (newName == None) {
+                console.log('No name available for role: harvester');
+                return;
+            }
             console.log('Spawning new harvester: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
                 {memory: {role: 'harvester'}});
--- scripts/ctrl.upgraderCreator.js
+++ scripts/ctrl.upgraderCreator.js
@@ -1,15 +1,19 @@
 var nameManager = require('data.nameManager');
 
-var harvesterCreator = {
+var upgradeCreator = {
     create: function(maxCout) {
-        var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        if(harvesters.length < maxCout) {
-            var newName = nameManager.getName('harvester');
-            console.log('Spawning new harvester: ' + newName);
+        var upgrades = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrade');
+        if(upgrades.length < maxCout) {
+            var newName = nameManager.getName('upgrade');
+            if (newName == None) {
+                console.log('No name available for role: upgrade');
+                return;
+            }
+            console.log('Spawning new upgrade: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
-                {memory: {role: 'harvester'}});
+                {memory: {role: 'upgrade'}});
         }
     }
 }
 
-module.exports = harvesterCreator;
+module.exports = upgradeCreator;
