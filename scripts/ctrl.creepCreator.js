0e29fff6145bce0e172d05b6cb617b6ab2beae0c
u03013112 <u03013112@hotmail.com>
2023-11-08 18:04:36 +0800

    bug

--- scripts/ctrl.creepCreator.js
+++ scripts/ctrl.creepCreator.js
@@ -3,12 +3,12 @@ var nameManager = require('data.nameManager');
 var creepCreator = {
     create: function(maxCout,role) {
         var creeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
-        var spawns = _.filter(Game.spawns, (spawn) => spawn.spawning == false);
+        var spawns = _.filter(Game.spawns, (spawn) => spawn.spawning == null);
 
         if(creeps.length < maxCout && spawns.length > 0) {
             var newName = nameManager.getName(role);
             if (newName == '') {
-                console.log('No name available for role: harvester');
+                console.log('No name available for role: ' + role);
                 return;
             }
             console.log('Spawning new harvester: ' + newName);
