e064f99751fca3a909058ee9733af5623c700ec2
u03013112 <u03013112@hotmail.com>
2023-11-12 00:14:30 +0800

    repair

--- scripts/struct.tower.js
+++ scripts/struct.tower.js
@@ -7,6 +7,25 @@ var tower = {
             tower.attack(closestHostile);
         }
 
+        // 优先修复container
+        var damagedContainers = _.filter(damagedStructs, (structure) => {
+            return structure.structureType == STRUCTURE_CONTAINER && structure.hits < structure.hitsMax;
+        })
+        if(damagedContainers.length > 0){
+            tower.repair(damagedContainers[0]);
+            return;
+        }
+
+        // 修复除了rampart和wall以外的其他建筑
+        var damagedStructs = _.filter(damagedStructs, (structure) => {
+            return structure.structureType != STRUCTURE_WALL && structure.structureType != STRUCTURE_RAMPART && structure.hits < structure.hitsMax;
+        })
+        if(damagedStructs.length > 0){
+            tower.repair(damagedStructs[0]);
+            return;
+        }
+
+        // 修复rampart和wall
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
