d85a53c919bfe8b2d84077232fbde7ec4dfc6f9e
u03013112 <u03013112@hotmail.com>
2023-11-11 18:01:58 +0800

    debug

--- scripts/role.miner.js
+++ scripts/role.miner.js
@@ -42,7 +42,7 @@ var miner = {
             var lastChar = creep.name.slice(-1); // 获取名字的最后一个字符
             var index = parseInt(lastChar, 10); // 尝试将字符转换为数字
             
-            if (length(containers) > (index + 1)) {
+            if (containers.length > (index + 1)) {
                 creep.memory.targetId = containers[index].id;
             }else{
                 creep.say('❌ no target');
