735459657a84a773fad0c778e69e794ac4260d6a
u03013112 <u03013112@hotmail.com>
2023-11-14 18:32:18 +0800

    test

---
+++ scripts/ctrl.harvest.js
@@ -0,0 +1,53 @@
+var harvest = {
+    harvest: function(creep) {
+        // 先尝试找到container，或者tombstone，进行采集
+        // 暂时就近采集，负载之后再说吧
+        var tombstones = creep.room.find(FIND_TOMBSTONES, {
+            filter: (tombstone) => {
+                return tombstone.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
+            }
+        })
+        var containers = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return structure.structureType == STRUCTURE_CONTAINER &&
+                    structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0;
+            }
+        })
+        var target = null;
+        if (tombstones.length > 0) {
+            var tombstone = creep.pos.findClosestByPath(tombstones);
+            if (tombstone) {
+                target = tombstone;
+            }
+        }
+        if (containers.length > 0 && !target) {
+            var container = creep.pos.findClosestByPath(containers);
+            if (container) {
+                target = container;
+            }
+        }
+        if (target) {
+            if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+            return;
+        }
+        // 如果没有container，就去找source
+        var sources = creep.room.find(FIND_SOURCES);
+        if (sources.length > 0) {
+            var lastChar = creep.name.slice(-1); // 获取名字的最后一个字符
+            var index = parseInt(lastChar, 10); // 尝试将字符转换为数字
+            if (isNaN(index)) { // 如果转换失败，将index设为0
+                index = 0;
+            }
+            index = index % sources.length;
+            var source = sources[index];
+            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source);
+            }
+            return;
+        }
+    },
+};
+
+module.exports = harvest;
--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -1,4 +1,5 @@
 var source = require('ctrl.source');
+var harvest = require('ctrl.harvest');
 
 var roleHarvester = {
 
@@ -125,7 +126,73 @@ var roleHarvester = {
                 }
             }
         }
-    }
+    },
+    run3: function(creep) {
+        if (creep.memory.state === undefined) {
+            creep.memory.state = 'harvesting';
+        }
+
+        if (creep.memory.state === 'harvesting' && creep.store.getFreeCapacity() === 0) {
+            creep.memory.state = 'transfering';
+            creep.say('⚡ transfer');
+        } else if (creep.memory.state === 'transfering' && creep.store[RESOURCE_ENERGY] === 0) {
+            creep.memory.state = 'harvesting';
+            creep.say('🔄 harvest');
+        }
+
+        if (creep.memory.state === 'harvesting') {
+            harvest.harvest(creep);
+        }else if (creep.memory.state === 'transfering') {
+            var hostiles = creep.room.find(FIND_HOSTILE_CREEPS);
+            if(hostiles.length == 0) {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                if(targets.length > 0){
+                    var target = creep.pos.findClosestByPath(targets);
+                    if (target) {
+                        ret = creep.transfer(target, RESOURCE_ENERGY);
+                        if (ret == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                        }
+                        return;
+                    }
+                }
+            }
+
+            // 找不到存储目标，找塔
+            targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType === STRUCTURE_TOWER) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0){
+                var target = creep.pos.findClosestByPath(targets);
+                if (target) {
+                    ret = creep.transfer(target, RESOURCE_ENERGY);
+                    if (ret == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
+                    }
+                    return;
+                }
+            }
+
+            // 找不到存储目标，找storage
+            var storage = creep.room.storage;
+            if (storage && storage.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                ret = creep.transfer(storage, RESOURCE_ENERGY);
+                if (ret == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(storage, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
+                return;
+            }
+        }
+    },
+
 };
 
 module.exports = roleHarvester;
