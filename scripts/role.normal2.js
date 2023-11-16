d304764d74bcf6cfad054437866443445b1d2ff5
u03013112 <u03013112@hotmail.com>
2023-11-16 19:16:29 +0800

    123

--- scripts/role.normal2.js
+++ scripts/role.normal2.js
@@ -115,6 +115,55 @@ var normal2 = {
                     console.log('please set Memory.roomInfo["'+room.name+'"]["source2storagePos"] = {x:0,y:0} manually')
                 }
             }
+            if (creep.memory.role == 'source2link'){
+                var source2linkPos = Memory.roomInfo[room.name]['source2linkPos']
+                if(source2linkPos){
+                    // 先判断是否已经在目标位置
+                    if (creep.pos.x == source2linkPos.x && creep.pos.y == source2linkPos.y){
+                        // 在目标位置，开始工作
+                        // 如果自己还有空位，就去挖矿
+                        if (creep.store.getFreeCapacity() > 0){
+                            // 找到最近的source
+                            var sources = creep.room.find(FIND_SOURCES);
+                            if (sources.length > 0){
+                                var source = creep.pos.findClosestByPath(sources);
+                                if (source){
+                                    ret = creep.harvest(source);
+                                    if (ret != OK){
+                                        console.log('creep.harvest error:'+ret);
+                                    }
+                                }
+                            }
+                        }else{
+                            // 如果自己满了，就去link
+                            var links = creep.room.find(FIND_STRUCTURES,{
+                                filter: (structure) => {
+                                    return structure.structureType == STRUCTURE_LINK;
+                                }
+                            })
+                            if (links.length > 0){
+                                var link = creep.pos.findClosestByPath(links);
+                                if (link && link.store.getFreeCapacity() > 0){
+                                    ret = creep.transfer(link,RESOURCE_ENERGY);
+                                    if (ret != OK){
+                                        console.log('creep.transfer error:'+ret);
+                                    }
+                                }
+                            }
+                        }
+                    }else{
+                        creep.moveTo(source2linkPos.x,source2linkPos.y);
+                    }
+                }else{
+                    console.log('please set Memory.roomInfo["'+room.name+'"]["source2linkPos"] = {x:0,y:0} manually')
+                }
+            }
+            if (creep.memory.role == 'link2storage'){
+
+            }
+            if (creep.memory.role == 'storage2controller'){
+
+            }
         }
     },
 };
