26aae739772ff99064d2168875b5bc613df1bc20
u03013112 <u03013112@hotmail.com>
2023-11-20 12:15:49 +0800

    1

--- scripts/main.js
+++ scripts/main.js
@@ -21,9 +21,9 @@ module.exports.loop = function () {
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
-        // if(creep.memory.role == 'upgrader') {
-        //     roleUpgrader.run(creep);
-        // }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
         if(creep.memory.role == 'builder'){
             roleBuilder.run(creep)
         }
