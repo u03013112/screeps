4d5b66df12ce2c0eba34c43096ec3d8f4b36c34e
u03013112 <u03013112@hotmail.com>
2024-12-04 14:11:14 +0800

    1

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,6 @@ var status = require('ctrl.status');
 
 module.exports.loop = function () {
 
-    var rooms = Game.rooms;
-    for(var roomName in rooms){
-        var room = rooms[roomName];
-        status.update2(room);
-        creepCreator.update(room);
-    }
-    
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -54,5 +47,12 @@ module.exports.loop = function () {
     });
     for(var name in towers){
         tower.run(towers[name]);
+    }
+
+    var rooms = Game.rooms;
+    for(var roomName in rooms){
+        var room = rooms[roomName];
+        status.update2(room);
+        creepCreator.update(room);
     }
 }
